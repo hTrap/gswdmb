9b7c66a2db1168c1a7f86fa88086b0bfbf0c468c
htrap <htrapdev@gmail.com>
2015-02-13 17:34:48 +0000

    base template,blog views, templates and urls

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
-
+from .models import Post
+from django.views.generic import ListView, DetailView
 # Create your views here.
+class PublishedPostsMixin(object):
+    def get_queryset(self):
+        queryset = super(PublishedPostsMixin, self).get_queryset()
+        return queryset.filter(published=True)
+
+class PostListView(PublishedPostsMixin, ListView):
+    model = Post
+    
+class PostDetailView(PublishedPostsMixin, DetailView):
+    model = Post
+
