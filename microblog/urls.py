e3ca220f072a5f7d74c31463e87095c3cae817c6
htrap <htrapdev@gmail.com>
2015-02-13 19:24:16 +0000

    static urls

--- microblog/urls.py
+++ microblog/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from . import views
+from django.conf import settings
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'microblog.views.home', name='home'),
@@ -8,4 +9,10 @@ urlpatterns = patterns('',
     url(r"^$", views.HomepageView.as_view(), name="home"), 
     url(r"^blog/", include("blog.urls", namespace="blog")),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns('',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+            'document_root': settings.STATIC_ROOT
+                }),
 )
